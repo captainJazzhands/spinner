18ac8c7b502cd440fcc5c5c6bd150cbbd170d9f8
captainJazzhands <github.com@ironicallyenough.com>
2023-01-24 10:40:09 -0500

    Needed a back end

---
+++ StroopShop/Controllers/Swatches.cs
@@ -0,0 +1,13 @@
+namespace StroopShop.Controllers;
+
+public class Word
+{
+    public string PartOfSpeech { get; set; }
+
+    public string TheWord { get; set; }
+
+    public Word()
+    {
+        
+    }
+}
